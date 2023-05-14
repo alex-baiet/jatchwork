a202f50f3027d3edfc85af10036cb4f48544ebae
alex-baiet <alex.baiet3@gmail.com>
2023-05-14 10:01:32 +0200

    Press Q to quit

--- src/fr/jatchwork/control/ControlWindow.java
+++ src/fr/jatchwork/control/ControlWindow.java
@@ -2,6 +2,8 @@ package fr.jatchwork.control;
 
 import fr.umlv.zen5.ApplicationContext;
 import fr.umlv.zen5.Event;
+import fr.umlv.zen5.Event.Action;
+import fr.umlv.zen5.KeyboardKey;
 
 public class ControlWindow {
   private ControlWindow() { }
@@ -9,5 +11,9 @@ public class ControlWindow {
   public static void manageInput(ApplicationContext context) {
     Event event = context.pollOrWaitEvent(Long.MAX_VALUE);
     System.out.println(event);
+    
+    if (event.getAction() == Action.KEY_PRESSED && event.getKey() == KeyboardKey.Q) {
+      System.exit(0);
+    }
   }
 }
