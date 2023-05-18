7e8e8b957fe152f33335cb78ef8ed78590c370ea
alex-baiet <alex.baiet3@gmail.com>
2023-05-18 15:29:57 +0200

    Removed html javadoc from Git

--- docs/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fr.jatchwork"},{"l":"fr.jatchwork.control"},{"l":"fr.jatchwork.model"},{"l":"fr.jatchwork.view"}];updateSearchResults();
